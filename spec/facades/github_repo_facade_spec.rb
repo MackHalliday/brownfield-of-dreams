8c1d0637bdca641fa33aaca54ae10a12261d4383
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-08 22:00:34 -0600

    Update testing with correct keys

--- spec/facades/github_repo_facade_spec.rb
+++ spec/facades/github_repo_facade_spec.rb
@@ -15,6 +15,11 @@ describe GithubRepoFacade do
 
   it 'can return github repo data' do
     facade = GithubRepoFacade.new
+
+    json_response = File.open('./fixtures/git_hub_data.json')
+      stub_request(:get, "https://api.github.com/user/repos")
+      .to_return(status: 200, body: json_response )
+
     data = facade.repo_data
 
     expect(data).to be_a Array
