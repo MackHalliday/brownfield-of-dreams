cbcbf42d1b18296f1ad75073cbf2075e13ddd529
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-09 21:14:22 -0600

    Update testing with expected keys

--- spec/services/github_service_test_spec.rb
+++ spec/services/github_service_test_spec.rb
@@ -1,19 +1,33 @@
 require 'rails_helper'
 
-describe 'Github API service' do
-  it "User Github Data", :vcr do
+describe 'github api service' do
+  it "user github repo data", :vcr do
 
     user = create(:user, github_token: ENV["GITHUB_API_KEY"])
 
     service = GithubApiService.new
 
-    raw_data = service.get_user_data(user)
-    binding.pry
+    raw_data = service.get_user_repo_data(user)
 
     expect(service).to be_a(GithubApiService)
     expect(raw_data).to be_a(Array)
 
     expect(raw_data.first).to have_key(:name)
+    expect(raw_data.first).to have_key(:html_url)
+  end
+
+  it "user github following data", :vcr do
+
+    user = create(:user, github_token: ENV["GITHUB_API_KEY"])
+
+    service = GithubApiService.new
+
+    raw_data = service.get_user_following_data(user)
+
+    expect(service).to be_a(GithubApiService)
+    expect(raw_data).to be_a(Array)
+
+    expect(raw_data.first).to have_key(:login)
     expect(raw_data.first).to have_key(:html_url)
   end
 end
