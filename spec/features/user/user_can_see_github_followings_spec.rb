04580ee96f550ef1aa484b13eec3f1f5e359f596
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-09 22:49:01 -0600

    Update stubbing method and add visit dashboard path

--- spec/features/user/user_can_see_github_followings_spec.rb
+++ spec/features/user/user_can_see_github_followings_spec.rb
@@ -18,7 +18,10 @@ describe 'User Dashboard' do
     allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(user)
 
+    visit dashboard_path
+
     save_and_open_page
+
     expect(page).to have_css(".github-following")
 
     within ".github-following" do
