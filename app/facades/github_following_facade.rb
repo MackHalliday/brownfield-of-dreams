f461582294b1568092b72c5093c7f8bb1aa10588
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-09 22:47:11 -0600

    Remove methods that limit number of followings displyed

--- app/facades/github_following_facade.rb
+++ app/facades/github_following_facade.rb
@@ -7,17 +7,9 @@ class GithubFollowingFacade
   end
 
   # refactor to split method
-  def all_following_data
+  def following_data
     @service.get_user_following_data(@current_user).map do |following_hash|
       Following.new(following_hash)
     end
-  end
-
-  def limit_following_5
-    all_following_data[0..4]
-  end
-
-  def following_data
-    limit_following_5
   end
 end
