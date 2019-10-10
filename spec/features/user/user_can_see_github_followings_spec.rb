9aeaec50daf192784dc374ac8700c9a4de5fa6db
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-09 22:39:24 -0600

    Update spec with stubbing for json responses

--- spec/features/user/user_can_see_github_followings_spec.rb
+++ spec/features/user/user_can_see_github_followings_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
 describe 'User Dashboard' do
-  it 'displays Github repos' do
-    skip
-    json_response = File.open('./spec/fixtures/github_following_view_data.json')
-
-    stub_request(:get, "https://api.github.com/user/repos")
-      .to_return(status: 200, body: json_response)
+  it 'displays github user following' do
 
     user = create(:user, github_token: ENV["GITHUB_API_KEY"])
+
+    json_following_response = File.open('./spec/fixtures/github_following_data.json')
+
+    stub_request(:get, "https://api.github.com/user/following")
+      .to_return(status: 200, body: json_following_response)
+
+    json_repo_response = File.open('./spec/fixtures/github_data.json')
+
+    stub_request(:get, "https://api.github.com/user/repos")
+    .to_return(status: 200, body: json_repo_response)
 
     allow_any_instance_of(ApplicationController)
       .to receive(:current_user).and_return(user)
 
+    save_and_open_page
     expect(page).to have_css(".github-following")
 
     within ".github-following" do
