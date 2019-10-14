f870e38193962d46183992935ef8a238d2f0d6b4
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-14 07:03:00 -0600

    test for visitor using bookmarks

--- spec/features/vistors/visitor_receives_flash_notice_when_bookmark_spec.rb
+++ spec/features/vistors/visitor_receives_flash_notice_when_bookmark_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'visitor sees a video show' do
+
   it 'vistor clicks on a tutorial title from the home page' do
     tutorial = create(:tutorial)
     video = create(:video, tutorial_id: tutorial.id)
@@ -9,8 +10,9 @@ describe 'visitor sees a video show' do
 
     click_on tutorial.title
 
-    expect(page).to have_current_path(tutorial_path(tutorial), ignore_query: true)
-    expect(page).to have_content(video.title)
-    expect(page).to have_content(tutorial.title)
+    click_link 'Bookmark'
+
+    expect(current_path).to eq(tutorial_path(tutorial))
+    expect(page).to have_content("User must login to bookmark videos.")
   end
 end
